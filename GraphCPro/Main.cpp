de617413ca4e255994095eb24b913baf64ad1ec4
Elijah <memory7734@gmail.com>
2016-04-28 22:47:04 +0800

    Create Main.cpp

---
+++ GraphCPro/Main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include"Tourism.h"
+using namespace std;
+
+int main(void)
+{
+	int bRunning=1;
+	while (bRunning)
+	{
+		//输出界面
+		cout << "====景区信息管理系统====" << endl;
+		cout << "1.创建景区景点图" << endl;
+		cout << "2.查询景点信息" << endl;
+		cout << "3.旅游景点导航" << endl;
+		cout << "4.搜索最短路径" << endl;
+		cout << "5.铺设电路规划" << endl;
+		cout << "0.退出" << endl;
+		cout << "请输入操作编号（0~5）：";
+		cin >> bRunning;
+		switch (bRunning)
+		{
+		case 1:
+			CreateGraph();
+			break;
+		case 2:
+			GetSpotInfo();
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 0:
+			break;
+		default:
+			break;
+		}
+	}
+}
